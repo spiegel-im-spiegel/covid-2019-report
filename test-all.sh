a4e237ea1d48dbb190682030fbe2f03498a1869c
Spiegel <spiegel.im.spiegel@gmail.com>
2021-01-02 15:33:12 +0900

    Update reports

--- test-all.sh
+++ test-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 go mod verify || exit 1
 go mod tidy -v || exit 1
-depm list -j | nancy sleuth -n || exit 1
+depm list -j | docker run --rm -i sonatypecommunity/nancy:latest sleuth -n || exit 1
 golangci-lint run --enable gosec ./... || exit 1
 go test ./... || exit 1
 depm m --dot --dot-config dot-config.toml | dot -Tpng -o dependency.png
