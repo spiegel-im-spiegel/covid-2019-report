ae3f4ee6217572202c04a175edb31857550d3d5d
Spiegel <spiegel.im.spiegel@gmail.com>
2021-04-18 21:56:40 +0900

    Add Taskfile.yml

--- build-chart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pushd report/
-./get-csv.sh
-popd
-# go run main.go report/WHO-COVID-19-japan-data.csv report/tokyo/130001_tokyo_covid19_patients.csv
-go run .
--- publish.sh
+++ publish.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-./build-chart.sh || exit 1
 comment="Update report in $(date -u '+%F')"
 git add --all || exit 1
 git commit -v -m "$comment" || exit 1
