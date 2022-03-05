120dd38c7613785a645389c97d2dd2f676b181a3
Spiegel <spiegel.im.spiegel@gmail.com>
2022-03-05 20:12:20 +0900

    Update reports

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func main() {
 		return
 	}
 	// experimental
-	if err := makeGraph(values.NewDate(2021, time.Month(10), 1), lastDay, HistCasesFileShort, HistDeathsFileShort); err != nil {
+	if err := makeGraph(lastDay.AddDay(-7*26), lastDay, HistCasesFileShort, HistDeathsFileShort); err != nil {
 		fmt.Fprintf(os.Stderr, "%+v\n", err)
 		return
 	}
